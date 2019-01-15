74caab4875346e2c07ed24fff470577c117f70d2
jheinz1999 <jonathan.heinz@aol.com>
2019-01-15 15:45:26 -0800

    calculation to find flushes, straights, and straight flushes implemented

--- games/poker/solver.js
+++ games/poker/solver.js
@@ -3,6 +3,9 @@ function solver(hand) {
   if (checkRoyalFlush(hand))
     return 9;
 
+  if (checkStraightFlush(hand))
+    return 8;
+
   return 0;
 
 }
@@ -27,6 +30,86 @@ function checkRoyalFlush(hand) {
     }
 
     return true;
+
+  }
+
+  return false;
+
+}
+
+function checkStraightFlush(hand) {
+
+  if (checkStraight(hand)) {
+
+    return checkFlush(hand);
+
+  }
+
+  else {
+
+    return false;
+
+  }
+
+}
+
+function checkStraight(hand) {
+
+  const cards = hand.map(card => card.value);
+  cards.sort().reverse();
+
+  let checking = true;
+  let startingIndex = 1;
+
+  while (checking) {
+
+    let count = 1;
+
+    for (let i = startingIndex; i < startingIndex + 4; i++) {
+
+      if (i === cards.length)
+        return false;
+
+      if (cards[i] + 1 !== cards[i - 1]) {
+
+        startingIndex++;
+        break;
+
+      }
+
+      count++;
+
+    }
+
+    if (count === 5)
+      checking = false;
+
+  }
+
+  console.log('highest straight: starts w/ ', cards[startingIndex - 1]);
+  return true;
+
+}
+
+function checkFlush(hand) {
+
+  const suits = hand.map(card => card.suit);
+
+  const counts = {
+
+    'S': 0,
+    'D': 0,
+    'C': 0,
+    'H': 0
+
+  }
+
+  for (let i = 0; i < suits.length; i++) {
+
+    counts[suits[i]]++;
+
+    if (counts[suits[i]] === 5)
+      return true;
 
   }
 
