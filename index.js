1bf9083491316320c0ac87a1a530734cccf004af
jheinz1999 <jonathan.heinz@aol.com>
2019-01-15 14:40:59 -0800

    Added Card constructor

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const Card = require('./cards/Card');
 
 const server = express();
 
 server.use(express.json());
+
+const AceofSpades = new Card('A', 'S');
+
+console.log(AceofSpades.card, AceofSpades.suit);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server live on port ${port}`));
