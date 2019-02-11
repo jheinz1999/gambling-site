649ac8488490cac61a12f2dc2bfef343b7430d38
jheinz1999 <jonathan.heinz@aol.com>
2019-02-11 13:19:22 -0800

    set up react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
