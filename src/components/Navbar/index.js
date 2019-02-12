679c2c1ecba02ad64176b28d6abb00f34c2fc20a
jheinz1999 <jonathan.heinz@aol.com>
2019-02-11 22:40:01 -0800

    set up navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Navbar.scss';
+
+export default function Navbar() {
+
+  return (
+
+    <nav>
+
+      <div className='left-group'>
+
+        <NavLink exact to='/' activeClassName='active'>Gambling Site</NavLink>
+
+      </div>
+
+      <div className='middle-group'>
+
+        <NavLink exact to='/poker' activeClassName='active'>Play Poker</NavLink>
+        <NavLink exact to='/blackjack' activeClassName='active'>Play Blackjack</NavLink>
+
+      </div>
+
+      <div className='right-group'>
+
+        <NavLink exact to='/login' activeClassName='active'>Sign Up/Log In</NavLink>
+
+      </div>
+
+    </nav>
+
+  );
+
+}
