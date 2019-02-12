bb591c94b0d8af5533633e77a9ccdeb6df7d46fa
jheinz1999 <jonathan.heinz@aol.com>
2019-02-11 22:40:01 -0800

    set up navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Navbar from './components/Navbar';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Test</h1>
+        <Navbar />
       </div>
     );
   }
