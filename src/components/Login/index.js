362c68e9814f558520fc4190f330c41b071cdab5
jheinz1999 <jonathan.heinz@aol.com>
2019-02-12 11:12:02 -0800

    added login page

---
+++ src/components/Login/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+
+  constructor() {
+
+    super();
+
+  }
+
+  render() {
+
+    return (
+
+      <h1>Form</h1>
+
+    );
+
+  }
+
+}
