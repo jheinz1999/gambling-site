2c2fe0bd5e5a9324af35b835adea5dffabcbe4f2
jheinz1999 <jonathan.heinz@aol.com>
2019-02-12 11:12:02 -0800

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+
+        <Route
+          path='/'
+          render={() => <Navbar />}
+        />
+
+
+        <Route
+          exact
+          path='/login'
+          render={props => <Login {...props} />}
+        />
+
       </div>
     );
   }
