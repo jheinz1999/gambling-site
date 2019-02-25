746241e5c965cabea9ba393df79e62f715792b53
Jonathan Heinz <jonathan.heinz@aol.com>
2019-02-24 23:39:11 -0800

    set up game system

---
+++ src/components/PokerGame/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './PokerGame.scss';
+
+class PokerGame extends React.Component {
+
+  constructor() {
+
+    super();
+
+    this.state = {
+
+      joined: false,
+      allReady: false,
+      users: null,
+      user: null
+
+    }
+
+  }
+
+  componentDidMount() {
+
+    this.setState({
+      user: JSON.parse(localStorage.user),
+      name: this.props.match.params.name
+    }, this.setupSocket);
+
+  }
+
+  componentWillUnmount() {
+
+    this.props.socket.emit('leftRoom', this.props.match.params.name);
+
+    this.props.socket.off();
+
+  }
+
+  setupSocket() {
+
+    const { socket } = this.props;
+
+    socket.emit('roomReq', this.props.match.params.name);
+    socket.emit('readyToStart', this.state.user.user_id);
+
+    socket.on('room', room => {
+
+      if (!room.users.find(user => user.username === this.state.user.username)) {
+
+        console.error('you aint in this room foo');
+
+      }
+
+      else {
+
+        this.setState({joined: true, users: room.users});
+
+      }
+
+    });
+
+    socket.on('allReady', () => {
+
+      this.setState({allReady: true});
+
+    })
+
+  }
+
+  render() {
+
+    const { joined, allReady } = this.state;
+
+    if (!joined)
+      return <h1>Joining room...</h1>
+
+    if (!allReady)
+      return <h1>Waiting for players...</h1>
+
+    return (
+
+      <div className='poker-game'>
+
+        <h1>poker</h1>
+
+      </div>
+
+    );
+
+  }
+
+}
+
+const stateToProps = state => {
+
+  return {
+
+    socket: state.socket
+
+  }
+
+}
+
+export default connect(stateToProps, null)(PokerGame);
