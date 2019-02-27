14cb23a350488ba268405124c65a6e367395d075
jheinz1999 <jonathan.heinz@aol.com>
2019-02-27 13:17:51 -0800

    made other pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Signup from './components/Signup';
 import PokerLobby from './components/PokerLobby';
 import PokerWaitingRoom from './components/PokerWaitingRoom';
 import PokerGame from './components/PokerGame';
+import LandingPage from './components/LandingPage';
+import BlackjackLobby from './components/BlackjackLobby';
 
 class App extends Component {
   render() {
@@ -16,6 +18,12 @@ class App extends Component {
         <Route
           path='/'
           render={() => <Navbar />}
+        />
+
+        <Route
+          exact
+          path='/'
+          render={() => <LandingPage />}
         />
 
         <Route
@@ -46,6 +54,12 @@ class App extends Component {
           exact
           path='/poker/room/:name/play'
           render={props => <PokerGame {...props} />}
+        />
+
+        <Route
+          exact
+          path='/blackjack'
+          render={props => <BlackjackLobby {...props} />}
         />
 
       </div>
