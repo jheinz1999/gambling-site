45870d1ebd4646202642df46ad159946965967e3
jheinz1999 <jonathan.heinz@aol.com>
2019-02-27 13:17:51 -0800

    made other pages

---
+++ frontend/src/components/BlackjackLobby/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function BlackjackLobby() {
+
+  return (
+
+    <h1>Blackjack</h1>
+
+  );
+
+}
---
+++ frontend/src/components/LandingPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './LandingPage.scss';
+
+export default function LandingPage() {
+
+  return (
+
+    <div className='landing'>
+
+      <h1>The Landing Page</h1>
+
+      <p>There will be some shizz here at some point, but for now it's just this lame text you're looking at. Sorry homies.</p>
+
+    </div>
+
+  );
+
+}
