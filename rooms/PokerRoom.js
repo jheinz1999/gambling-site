05946284f880eb6a52404577bd9fe4ea312cfdb0
jheinz1999 <jonathan.heinz@aol.com>
2019-03-04 23:15:24 -0800

    cards are properly dealt

--- rooms/PokerRoom.js
+++ rooms/PokerRoom.js
@@ -1,16 +1,29 @@
 const Room = require('./Room');
+const Deck = require('../cards/Deck');
+const { getIO } = require('../common/globals');
 
 class PokerRoom extends Room {
 
   constructor(name, leader) {
 
     super(name, leader);
+    this.hands = [];
+    this.deck = new Deck();
+    this.pot = 0;
+    this.turn = 0;
 
   }
 
   startPlaying() {
 
     super.startPlaying();
+
+    for (let i = 0; i < this.users.length; i++) {
+
+      this.hands.push(this.deck.draw(2));
+      getIO().to(this.users[i].username).emit('newHand', this.hands[i]);
+
+    }
 
   }
 
