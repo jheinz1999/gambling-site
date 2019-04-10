121cb4e42d9e8854dc3414e5a21bf5680a434c76
jheinz1999 <jonathan.heinz@aol.com>
2019-04-10 14:46:26 -0700

    now displaying users hand better

---
+++ frontend/src/components/PokerGame/Cards.js
@@ -0,0 +1,109 @@
+import _A_S from './cards/_A_S.png';
+import _2_S from './cards/_2_S.png';
+import _3_S from './cards/_3_S.png';
+import _4_S from './cards/_4_S.png';
+import _5_S from './cards/_5_S.png';
+import _6_S from './cards/_6_S.png';
+import _7_S from './cards/_7_S.png';
+import _8_S from './cards/_8_S.png';
+import _9_S from './cards/_9_S.png';
+import _10_S from './cards/_10_S.png';
+import _J_S from './cards/_J_S.png';
+import _Q_S from './cards/_Q_S.png';
+import _K_S from './cards/_K_S.png';
+import _A_C from './cards/_A_C.png';
+import _2_C from './cards/_2_C.png';
+import _3_C from './cards/_3_C.png';
+import _4_C from './cards/_4_C.png';
+import _5_C from './cards/_5_C.png';
+import _6_C from './cards/_6_C.png';
+import _7_C from './cards/_7_C.png';
+import _8_C from './cards/_8_C.png';
+import _9_C from './cards/_9_C.png';
+import _10_C from './cards/_10_C.png';
+import _J_C from './cards/_J_C.png';
+import _Q_C from './cards/_Q_C.png';
+import _K_C from './cards/_K_C.png';
+import _A_D from './cards/_A_D.png';
+import _2_D from './cards/_2_D.png';
+import _3_D from './cards/_3_D.png';
+import _4_D from './cards/_4_D.png';
+import _5_D from './cards/_5_D.png';
+import _6_D from './cards/_6_D.png';
+import _7_D from './cards/_7_D.png';
+import _8_D from './cards/_8_D.png';
+import _9_D from './cards/_9_D.png';
+import _10_D from './cards/_10_D.png';
+import _J_D from './cards/_J_D.png';
+import _Q_D from './cards/_Q_D.png';
+import _K_D from './cards/_K_D.png';
+import _A_H from './cards/_A_H.png';
+import _2_H from './cards/_2_H.png';
+import _3_H from './cards/_3_H.png';
+import _4_H from './cards/_4_H.png';
+import _5_H from './cards/_5_H.png';
+import _6_H from './cards/_6_H.png';
+import _7_H from './cards/_7_H.png';
+import _8_H from './cards/_8_H.png';
+import _9_H from './cards/_9_H.png';
+import _10_H from './cards/_10_H.png';
+import _J_H from './cards/_J_H.png';
+import _Q_H from './cards/_Q_H.png';
+import _K_H from './cards/_K_H.png';
+
+export default {
+
+  _A_S,
+  _2_S,
+  _3_S,
+  _4_S,
+  _5_S,
+  _6_S,
+  _7_S,
+  _8_S,
+  _9_S,
+  _10_S,
+  _J_S,
+  _Q_S,
+  _K_S,
+  _A_H,
+  _2_H,
+  _3_H,
+  _4_H,
+  _5_H,
+  _6_H,
+  _7_H,
+  _8_H,
+  _9_H,
+  _10_H,
+  _J_H,
+  _Q_H,
+  _K_H,
+  _A_C,
+  _2_C,
+  _3_C,
+  _4_C,
+  _5_C,
+  _6_C,
+  _7_C,
+  _8_C,
+  _9_C,
+  _10_C,
+  _J_C,
+  _Q_C,
+  _K_C,
+  _A_D,
+  _2_D,
+  _3_D,
+  _4_D,
+  _5_D,
+  _6_D,
+  _7_D,
+  _8_D,
+  _9_D,
+  _10_D,
+  _J_D,
+  _Q_D,
+  _K_D,
+
+}
